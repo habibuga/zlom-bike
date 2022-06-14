dd480a986b34b4eb9cf693b700fb67f57a3e2d3b
habibuga <vitaliy.ryzhynsky@gmail.com>
2022-06-14 16:31:38 +0200

    tests added

--- bike_app/tests.py
+++ bike_app/tests.py
@@ -1,6 +1,6 @@
 import pytest
-from .models import Category
 
+from django.contrib.auth.models import User
 
 @pytest.mark.django_db
 def test_category_view(client, categories, db):
@@ -10,3 +10,19 @@ def test_category_view(client, categories, db):
     for category in response.context['categories']:
         cat_list.append(category)
     assert len(cat_list) == 3
+
+
+@pytest.mark.django_db
+def test_user_detail_view(client, test_user, db):
+    response = client.get('/moj_profil/')
+    assert response.status_code == 200 or response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_user_detail_update(client, test_user, db):
+    response = client.post('/zmien_dane/', {"first_name": "first_tester", "last_name": "last_tester",
+                                            "email": "tester@tester.com"})
+    assert response.status_code == 200 or response.status_code == 302
+    # user_tester = User.objects.get(username="Tester")
+    # assert user_tester.first_name == "first_tester" and user_tester.last_name == "last_tester" \
+    #        and user_tester.email == "tester@tester.com"
--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from django.test import Client
 
 from bike_app.models import Offer, Category
+from django.contrib.auth.models import User
 
 
 @pytest.fixture
@@ -16,3 +17,14 @@ def categories():
     Category.objects.create(name="kategoria random", slug="kategoria-random")
     Category.objects.create(name="kategoria dummy", slug="kategoria-dummy")
 
+
+@pytest.fixture
+def test_user():
+    user_tester = User.objects.create(username="Tester")
+
+
+# @pytest.fixture
+# def offer():
+#     Offer.objects.create(name="jakaś oferta", description="jakiś opis", price=99.9,
+#                          category=Category.objects.get(slug="kategoria-testowa"),
+#                          user=User.objects.get(username="Tester")
